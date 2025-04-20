9ed603b7dea022dca0e49668447d40034450ab96
alkambia2019 <alk@sedulen.com>
2025-04-20 17:05:21 +0800

    Fix BoardingTime and FlightStatus

--- FidsCodingAssignment/Utils/CreateFlightUtility.cs
+++ FidsCodingAssignment/Utils/CreateFlightUtility.cs
@@ -12,6 +12,7 @@ namespace FidsCodingAssignment.Utils
             {
                 FlightId = flightdata.FlightId,
                 SchedTime = flightdata.SchedTime,
+                //Note: Base on json data if actualtime is null, estimatedtime has value with remarks either late or early.
                 ActualTime = flightdata.ActualTime.HasValue ? flightdata.ActualTime : flightdata.EstimatedTime,
                 AirLineCode = flightdata.AirLineCode,
                 FlightNumber = flightdata.FlightNumber,
@@ -26,16 +27,15 @@ namespace FidsCodingAssignment.Utils
             {
                 var boardingWindowInMinutes = int.Parse(configuration["Flight:BoardingWindowInMinutes"]);
                 var thresholdInMinutes = int.Parse(configuration["Flight:ThresholdInMinutes"]);
-                //CurrentTime
-                var predefinedWindow = flight.ActualTime.HasValue ? flight.ActualTime.Value.AddMinutes(boardingWindowInMinutes) 
-                    : DateTime.Now.AddMinutes(boardingWindowInMinutes);
-
-                if(DateTime.Now <= predefinedWindow || flight.ActualTime < DateTime.Now)
+                var timespanx =  flight.ActualTime - flight.SchedTime;
+                //CurrentTime? I assume its the current time
+                if (flight.ActualTime < DateTime.Now || flight.ActualTime >= (DateTime.Now.AddMinutes(-boardingWindowInMinutes)))
                 {
                     flight.BoardingTime = true;
                     flight.FlightStatus = "Boarding";
                 }
-                else
+
+                if (flight.ActualTime > (DateTime.Now.AddMinutes(thresholdInMinutes)))
                 {
                     flight.FlightStatus = "Closed";
                 }
